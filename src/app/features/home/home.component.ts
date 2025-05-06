f714918a29083917744acb0b78c2734fe40f2760
luis.brandon <luisbrandonbarrones@gmail.com>
2025-05-06 17:28:51 +0200

    Added home layout. WIP

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CardsContainerComponent } from "../cards-container/cards-container.component";
+import { FiltersContainerComponent } from "../filters-container/filters-container.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CardsContainerComponent, FiltersContainerComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
